8ce02f2816a16938f58dafc6636df17d5782849d
Pablo Arias <pabloarias.ic@gmail.com>
2024-12-11 08:33:17 -0300

    Initial Commit

---
+++ Test1WanderGallery/Test1WanderGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test1WanderGalleryApp.swift
+//  Test1WanderGallery
+//
+//  Created by Pablo on 11-12-24.
+//
+
+import SwiftUI
+
+@main
+struct Test1WanderGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
